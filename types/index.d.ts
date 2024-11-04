6b05ed56ac07c47ed24ed8c359068ac27b895c59
nraj07054 <nraj07054@gmail.com>
2024-11-04 15:47:05 +0530

    implement clerk webhooks

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,20 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 
 // ====== USER PARAMS
 declare type CreateUserParams = {
   clerkId: string;
   email: string;
-  username: string;
-  firstName: string | null;
-  lastName: string | null;
+  username?: string | null;
+  firstName?: string | null;
+  lastName?: string | null;
   photo: string;
 };
 
 declare type UpdateUserParams = {
-  firstName: string | null;
-  lastName: string | null;
-  username: string;
+  firstName?: string | null;
+  lastName?: string | null;
+  username?: string | null;
   photo: string;
 };
 
